435b88517b6416f5401062205a691e32113660d8
paul martin <mussym@live.com>
2020-07-17 16:14:25 -0500

    not sure

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-console.log('hey there!');
+var Greeting = /** @class */ (function () {
+    function Greeting() {
+    }
+    Greeting.prototype.greet = function () {
+        return "hello there";
+    };
+    return Greeting;
+}());
+var num = 12;
+console.log("number: " + num);
+var greet = new Greeting();
+console.log(greet.greet());
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -5,8 +5,19 @@ class Greeting {
     }
 }
 
-var num: number = 12
+const num: number = 12
 console.log(`number: ${num}`)
 
-var greet = new Greeting();
+const greet = new Greeting();
 console.log(greet.greet())
+
+//destructuring
+let input = [1, 2];
+let [first, second] = input;
+console.log(`First:${first}, Second:${second}`);
+
+function destuct([first, second]: [number, number]){
+    console.log(`First:${first}, Second:${second}`);
+}
+
+destuct([1, 2]);
